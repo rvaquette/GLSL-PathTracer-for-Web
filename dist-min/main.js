6d3cfb86fa3dc6248f78c7f03b155fa5d931686a
rvaquette <regisvaquette@gmail.com>
2026-08-06 22:33:37 +0200

    step

--- dist-min/main.js
+++ dist-min/main.js
@@ -1,1 +1,1 @@
-import{b as a}from"./chunks/chunk-4HHU5DK5.js";import"./chunks/chunk-Q4WHUCDY.js";import"./chunks/chunk-ANN5UYCF.js";export{a as Main};
+import{b as a}from"./chunks/chunk-S3ZKDQ6U.js";import"./chunks/chunk-Q4WHUCDY.js";import"./chunks/chunk-ANN5UYCF.js";export{a as Main};
