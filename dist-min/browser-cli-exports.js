3f98d801d71acf1928939c2966c2d43f920b461d
rvaquette <regisvaquette@gmail.com>
2026-09-02 14:18:23 +0200

    step

--- dist-min/browser-cli-exports.js
+++ dist-min/browser-cli-exports.js
@@ -1,1 +1,1 @@
-import{a as o}from"./chunks/chunk-UZ4LEDWF.js";import"./chunks/chunk-Q4WHUCDY.js";import"./chunks/chunk-ANN5UYCF.js";export{o as Controls};
+import{a as o}from"./chunks/chunk-X46DNZ3I.js";import"./chunks/chunk-Q4WHUCDY.js";import"./chunks/chunk-ANN5UYCF.js";export{o as Controls};
--- dist-min/main.js
+++ dist-min/main.js
@@ -1,1 +1,1 @@
-import{b as a}from"./chunks/chunk-UZ4LEDWF.js";import"./chunks/chunk-Q4WHUCDY.js";import"./chunks/chunk-ANN5UYCF.js";export{a as Main};
+import{b as a}from"./chunks/chunk-X46DNZ3I.js";import"./chunks/chunk-Q4WHUCDY.js";import"./chunks/chunk-ANN5UYCF.js";export{a as Main};
